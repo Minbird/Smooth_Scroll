18766b47d7a5b9d0d9ea7a006b108fcc3fa9ac7d
Minbird <33375522+Minbird@users.noreply.github.com>
2021-08-01 11:01:53 +0900

    1.4
    Fixed an issue that scroll doesn't smooth but clattered when on low fps.

--- smooth_scroll/lua/autorun/client/smooth_scroll_startup.lua
+++ smooth_scroll/lua/autorun/client/smooth_scroll_startup.lua
@@ -11,9 +11,22 @@ hook.Add( "PreGamemodeLoaded", "Reloaded_DVScrollBar_Control", function()
 	print("//    Smooth Scroll StartUp    //")
 	print("/////////////////////////////////")
 	print("By Minbird")
-	print("System Version: 1.3")
-	print("System Version Date: 2021.7.30")
+	print("System Version: 1.4")
+	print("System Version Date: 2021.8.1")
 	print("[Smooth Scroll] Trying to redefine controls...")
+	
+	local function sign( num )
+		return num > 0
+	end
+	
+	local function getBiggerPos( signOld, signNew, old, new )
+		if signOld != signNew then return new end
+		if signNew then
+			return math.max(old, new)
+		else
+			return math.min(old, new)
+		end
+	end
 
 	local dermaCtrs = vgui.GetControlTable( "DVScrollBar" )
 
@@ -21,6 +34,9 @@ hook.Add( "PreGamemodeLoaded", "Reloaded_DVScrollBar_Control", function()
 	local newerT = 0
 
 	function dermaCtrs:AddScroll( dlta )
+	
+		self.Old_Pos = nil
+		self.Old_Sign = nil
 
 		local OldScroll = self:GetScroll()
 
@@ -32,13 +48,20 @@ hook.Add( "PreGamemodeLoaded", "Reloaded_DVScrollBar_Control", function()
 		tScroll = tScroll + dlta
 
 		local ctime = RealTime() -- does not work correctly with CurTime, when in single player game and in game menu (then CurTime get stuck). I think RealTime is better.
+		local doing_scroll = true
 		newerT = ctime
 		
 		anim.Think = function( anim, pnl, fraction )
+			local nowpos = Lerp( fraction, anim.StartPos, anim.TargetPos )
 			if ctime == newerT then
-				self:SetScroll( Lerp( fraction, anim.StartPos, anim.TargetPos ) )
+				self:SetScroll( getBiggerPos( self.Old_Sign, sign(dlta), self.Old_Pos, nowpos ) )
 				tScroll = tScroll - (tScroll * fraction)
 			end
+			if doing_scroll then -- it must be here. if not, sometimes scroll get bounce.
+				self.Old_Sign = sign(dlta)
+				self.Old_Pos = nowpos
+			end
+			if ctime != newerT then doing_scroll = false end
 		end
 
 		return math.Clamp( self:GetScroll() + tScroll, 0, self.CanvasSize ) != self:GetScroll()
